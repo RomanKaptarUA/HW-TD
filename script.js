87471c2a40fff85b1c676cfbe97271d2a4a94478
RomanKaptarUA <romankaptar82@gmail.com>
2024-11-12 21:11:28 +0200

    /

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-// const text = document.querySelector('.text');
-// const plus = document.querySelector('.plus');
-// const minus = document.querySelector('.minus');
 
-// function updateWidth(change) {
-//    const curerntWidthSize = parseInt(window.getComputedStyle(text).width);
-//    text.style.fontSize = `${curerntWidthSize + change}px`; 
+
+//1
+
+// const image = document.querySelector(".slider__image");
+// const sliderInput = document.querySelector(".slider__input");
+
+// function resizeImage(event) {
+//   const sliderValue = event.target.value;
+//   const resize = sliderValue / 60;
+//   image.style.transform = `scale(${resize})`;
+
 // }
-// plus.addEventListener('click', () => {
-//     updateWidth(20);
-// });
-// minus.addEventListener('click', () => {
-//     updateWidth(-20);
-// })
+// const debounceUpdate = _.debounce(resizeImage, 50);
+
+// sliderInput.addEventListener("input", debounceUpdate);
 
 
 
-const image = document.querySelector(".slider__image");
-const sliderInput = document.querySelector(".slider__input");
 
-function resizeImage(event) {
-  const sliderValue = event.target.value;
-
-  const resize = sliderValue / 60;
-  image.style.transform = `scale(${resize})`;
+//2
 
 
-const debounceUpdate = _.debounce(resizeImage, 50);
-}
-sliderInput.addEventListener("input", resizeImage);
+function updateCoordinats(event) {
+  const graph = document.getElementById('graph');
+  const point = document.getElementById('point');
+  const coordDisplay = document.getElementById('coord');
+
+  const rect = graph.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+
+  point.setAttribute('cx', x);
+  point.setAttribute('cy', y);
+
+  coordDisplay.textContent = `X: ${x.toFixed(0)}, Y: ${y.toFixed(0)}`;
+};
+
+const throttleUpdate = _.debounce(updateCoordinats, 500);
+
+const graphEl = document.getElementById('graph');
+graphEl.addEventListener('mousemove', throttleUpdate);
